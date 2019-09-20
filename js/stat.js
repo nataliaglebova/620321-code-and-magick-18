cb6218a1242c0fb53807c30a725b50fb9f109fef
nataliaglebova7 <nataliaglebova7@gmail.com>
2019-09-20 18:15:11 +0300

    введение функции по рисованию облака и доработка цвета

--- js/stat.js
+++ js/stat.js
@@ -1,31 +1,24 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  // функция для рисования облака и тени
+  var paintCloudAndShadow = function (CLOUD_X, CLOUD_HIGHT, CLOUD_WIDTH, START_POINT, ANGLE_POINT, SHADOW_MOVE) {
+    ctx.beginPath();
+    ctx.moveTo(START_POINT, SHADOW_MOVE);
+    ctx.lineTo(CLOUD_X + SHADOW_MOVE, ANGLE_POINT + SHADOW_MOVE);
+    ctx.lineTo(CLOUD_X + SHADOW_MOVE, CLOUD_HIGHT + SHADOW_MOVE);
+    ctx.lineTo(CLOUD_X + CLOUD_WIDTH + SHADOW_MOVE, CLOUD_HIGHT + SHADOW_MOVE);
+    ctx.lineTo(CLOUD_X + CLOUD_WIDTH + SHADOW_MOVE, CLOUD_HIGHT - ANGLE_POINT + SHADOW_MOVE);
+    ctx.lineTo(CLOUD_X + CLOUD_WIDTH + SHADOW_MOVE, ANGLE_POINT + SHADOW_MOVE);
+    ctx.fill();
+  };
 
-  var CLOUD_X = 100;
-  var CLOUD_WIDTH = 420;
-  var CLOUD_HIGHT = 270;
-  // рисование тени облака со статистикой
+  // рисование тени облака
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.beginPath();
-  ctx.moveTo(320, 10);
-  ctx.lineTo(CLOUD_X + 10, 50);
-  ctx.lineTo(CLOUD_X + 10, CLOUD_HIGHT + 10);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH + 10, CLOUD_HIGHT + 10);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH + 10, 240);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH + 10, 50);
-  ctx.fill();
-
+  paintCloudAndShadow(100, 270, 420, 320, 40, 10);
   // рисование самого облака
   ctx.fillStyle = '#fff';
-  ctx.beginPath();
-  ctx.moveTo(310, 0);
-  ctx.lineTo(CLOUD_X, 40);
-  ctx.lineTo(CLOUD_X, CLOUD_HIGHT);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH, CLOUD_HIGHT);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH, 230);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH, 40);
-  ctx.fill();
+  paintCloudAndShadow(100, 270, 420, 320, 40, 0);
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
   ctx.fillText('Ура вы победили!', 220, 30);
@@ -48,8 +41,6 @@ window.renderStatistics = function (ctx, names, times) {
   var GRAPH_X = 150;
   var GRAPH_HIGHT = 150;
   var STAT_HIGHT = 240;
-  var saturation = Math.round(Math.random() * 100);
-  var hslColor = 'hsl(240,' + saturation + '%, 50%)';
 
   for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = 'black';
@@ -58,6 +49,8 @@ window.renderStatistics = function (ctx, names, times) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
+      var saturation = Math.round(Math.random() * 100);
+      var hslColor = 'hsl(240,' + saturation + '%, 50%)';
       ctx.fillStyle = hslColor;
     }
     ctx.fillRect(GRAPH_X + (GRAPH_WIDTH + GAP) * i, STAT_HIGHT - GRAPH_HIGHT * Math.round(times[i]) / maxTime, GRAPH_WIDTH, GRAPH_HIGHT * Math.round(times[i]) / maxTime);
