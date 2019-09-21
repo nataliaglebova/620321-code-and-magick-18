3ee71d6873d7f13fba36f6d72c7cfd64c517b54c
Natalia Glebova <nataliaglebova7@gmail.com>
2019-09-21 17:34:54 +0300

    Обновление функций, добаботка по замечаниям

--- js/stat.js
+++ js/stat.js
@@ -1,10 +1,30 @@
 'use strict';
-
 window.renderStatistics = function (ctx, names, times) {
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 220, 30);
-  ctx.fillText('Список результатов:', 210, 60);
+  var CLOUD_X = 100;
+  var CLOUD_HIGHT = 270;
+  var CLOUD_WIDTH = 420;
+  var START_POINT = 320;
+  var ANGLE_POINT = 50;
+  var SHADOW_MOVE = 10;
+  var CLOUD_COLOR = 'rgba(0, 0, 0, 0.7)';
+  // рисование тени
+  paintCloudAndShadow(CLOUD_X, CLOUD_HIGHT, CLOUD_WIDTH, START_POINT, ANGLE_POINT, SHADOW_MOVE, CLOUD_COLOR, ctx);
+  // рисование облака
+  SHADOW_MOVE = 0;
+  CLOUD_COLOR = '#FFF';
+  paintCloudAndShadow(CLOUD_X, CLOUD_HIGHT, CLOUD_WIDTH, START_POINT, ANGLE_POINT, SHADOW_MOVE, CLOUD_COLOR, ctx);
+
+  // надписи в облаке
+  var FONT_STYLE = '16px PT Mono';
+  var TEXT_COLOR = '#000';
+  var WINNER_TEXT = 'Ура вы победили!';
+  var WINNER_TEXT_X = 220;
+  var WINNER_TEXT_Y = 35;
+  var RESULT_TEXT = 'Список результатов:';
+  var RESULT_TEXT_X = 210;
+  var RESULT_TEXT_Y = 60;
+  printWelcomeWords(FONT_STYLE, TEXT_COLOR, WINNER_TEXT, WINNER_TEXT_X, WINNER_TEXT_Y, RESULT_TEXT, RESULT_TEXT_X, RESULT_TEXT_Y, ctx);
+
   // поиск максимального времени в массиве
   var getMaxElement = function (arr) {
     var maxElement = arr[0];
@@ -16,6 +36,7 @@ window.renderStatistics = function (ctx, names, times) {
     return maxElement;
   };
   var maxTime = getMaxElement(times);
+
   // отрисовка гистограммы
   var GAP = 50;
   var GRAPH_WIDTH = 40;
@@ -24,7 +45,7 @@ window.renderStatistics = function (ctx, names, times) {
   var STAT_HIGHT = 240;
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = '#000';
     ctx.fillText(names[i], GRAPH_X + (GRAPH_WIDTH + GAP) * i, STAT_HIGHT + GAP / 2);
     ctx.fillText(Math.round(times[i]), GRAPH_X + (GRAPH_WIDTH + GAP) * i, STAT_HIGHT - GRAPH_HIGHT * Math.round(times[i]) / maxTime - 10);
     if (names[i] === 'Вы') {
@@ -37,32 +58,24 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillRect(GRAPH_X + (GRAPH_WIDTH + GAP) * i, STAT_HIGHT - GRAPH_HIGHT * Math.round(times[i]) / maxTime, GRAPH_WIDTH, GRAPH_HIGHT * Math.round(times[i]) / maxTime);
   }
 };
+
 // функция для рисования облака и тени
-var paintCloudAndShadow = function (CLOUD_X, CLOUD_HIGHT, CLOUD_WIDTH, START_POINT, ANGLE_POINT, SHADOW_MOVE, cloudColor, ctx) {
+var paintCloudAndShadow = function (CLOUD_X, CLOUD_HIGHT, CLOUD_WIDTH, START_POINT, ANGLE_POINT, SHADOW_MOVE, CLOUD_COLOR, ctx) {
   ctx.beginPath();
   ctx.moveTo(START_POINT, SHADOW_MOVE);
-  ctx.lineTo(CLOUD_X + SHADOW_MOVE, ANGLE_POINT + SHADOW_MOVE);
-  ctx.lineTo(CLOUD_X + SHADOW_MOVE, CLOUD_HIGHT + SHADOW_MOVE);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH + SHADOW_MOVE, CLOUD_HIGHT + SHADOW_MOVE);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH + SHADOW_MOVE, CLOUD_HIGHT - ANGLE_POINT + SHADOW_MOVE);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH + SHADOW_MOVE, ANGLE_POINT + SHADOW_MOVE);
+  ctx.lineTo((CLOUD_X + SHADOW_MOVE), (ANGLE_POINT + SHADOW_MOVE));
+  ctx.lineTo((CLOUD_X + SHADOW_MOVE), CLOUD_HIGHT + SHADOW_MOVE);
+  ctx.lineTo((CLOUD_X + CLOUD_WIDTH + SHADOW_MOVE), (CLOUD_HIGHT + SHADOW_MOVE));
+  ctx.lineTo((CLOUD_X + CLOUD_WIDTH + SHADOW_MOVE), (CLOUD_HIGHT - ANGLE_POINT + SHADOW_MOVE));
+  ctx.lineTo((CLOUD_X + CLOUD_WIDTH + SHADOW_MOVE), (ANGLE_POINT + SHADOW_MOVE));
+  ctx.fillStyle = CLOUD_COLOR;
   ctx.fill();
-  ctx.fillStyle = cloudColor;
 };
 
-// рисование тени облака
-var CLOUD_X = 100;
-var CLOUD_HIGHT = 270;
-var CLOUD_WIDTH = 420;
-var START_POINT = 320;
-var ANGLE_POINT = 40;
-var SHADOW_MOVE = 10;
-var cloudColor = 'rgba(0, 0, 0, 0.7)';
-
-// вынужденное объявление , т.к.  иначе lint считает что переменная не определена
-var ctx;
-paintCloudAndShadow(CLOUD_X, CLOUD_HIGHT, CLOUD_WIDTH, START_POINT, ANGLE_POINT, SHADOW_MOVE, cloudColor, ctx);
-// рисование самого облака
-SHADOW_MOVE = 10;
-cloudColor = '#fff';
-paintCloudAndShadow(CLOUD_X, CLOUD_HIGHT, CLOUD_WIDTH, START_POINT, ANGLE_POINT, SHADOW_MOVE, cloudColor, ctx);
+// функция вывода текста в облаке
+var printWelcomeWords = function (FONT_STYLE, TEXT_COLOR, WINNER_TEXT, WINNER_TEXT_X, WINNER_TEXT_Y, RESULT_TEXT, RESULT_TEXT_X, RESULT_TEXT_Y, ctx) {
+  ctx.font = FONT_STYLE;
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.fillText(WINNER_TEXT, WINNER_TEXT_X, WINNER_TEXT_Y);
+  ctx.fillText(RESULT_TEXT, RESULT_TEXT_X, RESULT_TEXT_Y);
+};
